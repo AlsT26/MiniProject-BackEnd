1156fe7eed4c8462e54ae7079434ed89c3849b20
andhikarsurya <andhikarsurya@gmail.com>
2024-12-10 05:35:15 +0700

    adding ref code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,15 +5,26 @@ import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 export class UserController {
-    async createUser(req: Request, res: Response) {
-        try {
-          await prisma.user.create({ data: req.body });
-          res.status(201).send("User created ✅");
-        } catch (err) {
-          console.log(err);
-          res.status(400).send(err);
-        }
-      }
+  async createUser(req: Request, res: Response) {
+    try {
+      const newUser = await prisma.user.create({
+        data: req.body
+      });
+      const randomNum = Math.floor(1000 + Math.random() * 9000);
+      const refCode = `${newUser.id}${randomNum}`;
+      await prisma.user.update({
+        where: { id: newUser.id },
+        data: { ref_code: refCode }
+      });
+      console.log(`User created with ID: ${newUser.id}, ref_code: ${refCode}`);
+  
+      res.status(201).send("User created ✅");
+    } catch (err) {
+      console.error(err);
+      res.status(400).send(err);
+    }
+  }
+  
       async deleteUser(req: Request, res: Response) {
         try {
           const { id } = req.params;

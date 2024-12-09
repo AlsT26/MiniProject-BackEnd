9b68ace025306be95343f079f160993d7729d567
andhikarsurya <andhikarsurya@gmail.com>
2024-12-09 15:58:46 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { UserRouter } from "./routers/user.router";
 
 const PORT: number = 8000;
 
@@ -15,6 +16,8 @@ app.use(
 app.get("/ticketbooth", (req: Request, res: Response) => {
   res.status(200).send("TicketBooth API");
 });
+const userRouter = new UserRouter();
+app.use("/api/users", userRouter.getRouter());
 
 // app.use("/ticketbooth");
 

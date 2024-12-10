ad0b9e40808fb52d3932c0a3219bb1ee4ecb1504
alvintsibuea26 <101545197+AlsT26@users.noreply.github.com>
2024-12-10 13:23:32 +0700

    Ansurya (#4)
    * test
    
    * test
    
    * adding ref code
    
    * make auth
    
    * make auth
    
    * add auth
    
    * adding custom
    
    ---------
    
    Co-authored-by: andhikarsurya <andhikarsurya@gmail.com>

---
+++ src/custom.d.ts
@@ -0,0 +1,13 @@
+import { RoleUser } from "@prisma/client";
+import 'express'
+export type UserPayload ={
+    id:number;
+    role:RoleUser
+}
+declare global{
+    namespace Express{
+        export interface Request{
+            user?:UserPayload
+        }
+    }
+}

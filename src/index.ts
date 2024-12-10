981dad72f584c6f49fcfc07af881772c832a19ab
andhikarsurya <andhikarsurya@gmail.com>
2024-12-10 11:26:51 +0700

    add auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { UserRouter } from "./routers/user.router";
-
+import { AuthRouter } from "./routers/auth.router";
 const PORT: number = 8000;
 
 const app: Application = express();
@@ -17,6 +17,9 @@ app.get("/ticketbooth", (req: Request, res: Response) => {
   res.status(200).send("TicketBooth API");
 });
 const userRouter = new UserRouter();
+const authRouter = new AuthRouter();
+
+app.use("/api/auth", authRouter.getRouter());
 app.use("/api/users", userRouter.getRouter());
 
 app.listen(PORT, () => {

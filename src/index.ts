e96a30726711dc16858343a2e6c3cce75e3f0603
alvintsibuea26 <101545197+AlsT26@users.noreply.github.com>
2025-01-07 23:50:01 +0700

    Alvin (#26)
    * setting localhost server
    
    * making password changes
    
    * change database pw
    
    * merge
    
    * adding new controllers and routes
    
    * adding new event controllers features
    
    * adding new eventController
    
    * many changes & first deploy trial
    
    * first deploy
    
    * making order, add new event (still fail), making ticket (still fail)
    
    * Delete node_modules directory
    
    * same changes
    
    * trying to pull
    
    * saving changes
    
    * fixing verify
    
    * fix deploy
    
    * fix deploy
    
    * fix deploy
    
    * fix deploy
    
    * fix deploy
    
    * fix deploy
    
    * deploy
    
    * fix deploy

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     origin: process.env.BASE_URL_FE,
     credentials: true,
   })
 );
-app.use(cors());
-app.use(cookieParser());
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   res.status(200).send("welcome to api");
 });
 const userRouter = new UserRouter();

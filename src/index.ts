21dc3d61d3661e70aac9435704e1b0f5ff89d6a1
alvintsibuea26 <101545197+AlsT26@users.noreply.github.com>
2025-01-08 00:02:09 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(
     credentials: true,
   })
 );
-app.get("/api", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send("welcome to api");
 });
 const userRouter = new UserRouter();

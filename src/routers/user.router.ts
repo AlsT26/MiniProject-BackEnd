244f8dbe30541b27d828d613b3d47e8d344c6ac3
andhikarsurya <andhikarsurya@gmail.com>
2025-01-08 12:58:54 +0700

    hi

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
-import { checkAdmin, verifyToken } from "../middlewares/verify";
+import { verifyToken } from "../middlewares/verify";
 
 export class UserRouter {
   private userController: UserController;
